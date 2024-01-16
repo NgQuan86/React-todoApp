93279b0f0d51553b4156f0586db3267bc9cdeebb
NgQuan <nguyen.phuoc.hoang.quan@gmail.com>
2024-01-16 15:31:44 +0100

    import image

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Color from "./HOC/color";
+import logo from '../assets/images/pool-bar.jpg';
 
 class Home extends React.Component {
-    render(){
+    render() {
         console.log('check props', this.props)
-        return(
-            <div>
-                Hello Wolrd 
-            </div>
+        return (
+            <>
+                <div>
+                    Hello Wolrd
+                </div>
+                <div>
+                    <img src={logo} style={{width:'40vmin'}}/>
+                </div>
+            </>
         )
     }
 }
